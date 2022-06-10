2a810d6c84da9e28dfbec9d1f267ae3c8b529c01
TairSadyk <tairsadykov92@gmail.com>
2022-06-09 22:50:56 -0400

    Collections, research and prints section/pages are added

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-"strict";
-editorialsSection = document.querySelector(".editorials");
-collectionsSection = document.querySelector(".collections");
-sections = document.querySelectorAll("section");
+"use strict";
+const editorialsSection = document.querySelector(".editorials");
+const collectionsSection = document.querySelector(".collections");
+const sections = document.querySelectorAll("section");
 
-navLinks = document.querySelector(".nav__list");
+const navList = document.querySelector(".nav__list");
+const navLinks = document.querySelectorAll(".nav__link");
 
-navLinks.addEventListener("click", function (e) {
+navList.addEventListener("click", function (e) {
   const link = e.target.closest(".nav__link");
   if (!link || link.textContent === "contact") return;
   e.preventDefault();
-  console.log(link.textContent);
-  [...sections].forEach((sec) => {
-    !sec.classList.contains("hidden") && sec.classList.add("hidden");
-  });
+  [...navLinks].forEach((link) => link.classList.remove("active"));
+  !link.classList.contains("active") && link.classList.add("active");
+  [...sections].forEach(
+    (sec) => !sec.classList.contains("hidden") && sec.classList.add("hidden")
+  );
   document.querySelector(`.${link.textContent}`).classList.remove("hidden");
 });
