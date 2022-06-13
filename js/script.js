e8abf83c9506843fa6e9a6e72ab4d543c0caa47c
TairSadyk <tairsadykov92@gmail.com>
2022-06-12 22:38:49 -0400

    fixed header animation, refractored sass code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 "use strict";
-const editorialsSection = document.querySelector(".editorials");
-const collectionsSection = document.querySelector(".collections");
 const sections = document.querySelectorAll("section");
-
 const navList = document.querySelector(".nav__list");
 const navLinks = document.querySelectorAll(".nav__link");
-const allImgs = document.querySelectorAll(".portfolio-image");
 
 navList.addEventListener("click", function (e) {
   const link = e.target.closest(".nav__link");
@@ -25,20 +21,4 @@ navList.addEventListener("click", function (e) {
   document
     .querySelector("body")
     .scrollIntoView({ block: "start", behavior: "smooth" });
-});
-
-const imageReveal = function (entries, observer) {
-  const [entry] = entries;
-  console.log(entry);
-  // if (!entry.isIntersecting) return;
-  // entry.target.classList.remove("portfolio-image--hidden");
-  observer.unobserve(entry.target);
-};
-const imageObserver = new IntersectionObserver(imageReveal, {
-  root: null,
-  threshold: 0.15,
-});
-allImgs.forEach((img) => {
-  if (img.clientHeight !== 0) imageObserver.observe(img);
-  // img.classList.add("portfolio-image--hidden");
 });
